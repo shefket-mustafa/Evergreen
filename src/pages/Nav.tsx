267ed20d002167bb61da4cf6c6d87227fbe1a106
shefket-mustafa <miracle_1@abv.bg>
2025-06-18 23:32:09 +0300

    task/endpoints
    Endpoints added to every Link

--- src/pages/Nav.tsx
+++ src/pages/Nav.tsx
@@ -1,26 +1,29 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
-export default function Nav () {
+export default function Nav() {
 
+    const navigate = useNavigate();
 
+  return (
+    <div className="w-full bg-transparent flex flex-row justify-between px-20 lg:h-15 items-center">
+      <div
+        className="text-emerald-400 text-4xl"
+        style={{ fontFamily: "'Pacifico', cursive" }}
+      >
+        <Link to="/">EverGreen</Link>
+      </div>
 
+      <div className="flex gap-10 text-white">
+        <Link to="/about">About us</Link>
+        <Link to="/services">Our Services</Link>
+        <Link to="/treatments">Treatments</Link>
+        <Link to="/gallery">Gallery</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
 
-    return <div className="w-full bg-transparent flex flex-row justify-between px-20 lg:h-15 items-center">
-            <div className="text-emerald-400 text-4xl" style={{fontFamily: "'Pacifico', cursive"}} >
-                <p >EverGreen</p>
-            </div>
-
-            <div className="flex gap-10 text-white">
-                <Link to='/about'>About us</Link>
-                <Link to='/services'>Our Services</Link>
-                <Link to='/treatments'>Treatments</Link>
-                <Link to='/gallery'>Gallery</Link>
-                <Link to='/contact'>Contact</Link>
-            </div>
-
-            <div className="px-6 py-2 rounded-4xl bg-emerald-400">
-                <p >Request free Survey</p>
-            </div>
-        </div>
-    
+      <div onClick={() => navigate('/survey')} className="px-6 py-2 rounded-4xl bg-emerald-400 cursor-pointer">
+        <p>Request free Survey</p>
+      </div>
+    </div>
+  );
 }
