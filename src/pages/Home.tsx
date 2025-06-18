b08769faa0ede89ceffc38dd90b51ac0bcfaa9ad
shefket-mustafa <miracle_1@abv.bg>
2025-06-18 23:52:39 +0300

    fix/background-img-endpoint-fixed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 
 import Hero from "./Hero";
 import Nav from "./Nav";
+import Services from "./Services";
 
 export default function Home(){
 
 
-    return <div className="w-full bg-cover h-screen" style={{backgroundImage: "url('/public/images/background-hero.png')"}}>
+    return <div className="w-full bg-cover h-screen overflow-y-auto" style={{backgroundImage: "url('/images/background-hero.png')"}}>
     <Nav />
     <Hero />
+    <Services />
     </div>
 }
---
+++ src/pages/Services.tsx
@@ -0,0 +1,10 @@
+export default function Services() {
+
+
+    return <div>
+
+        {/* left container  */}
+        <div className="lg:w-1/2 h-64 bg-cover bg-center" style={{backgroundImage: "url('/public/images/services.png')"}}>
+        </div>
+    </div>
+}
