abc3ba3ed97f2c5b5ff173938bfd3c37d6dd7941
shefket-mustafa <miracle_1@abv.bg>
2025-07-10 23:04:52 +0300

    feat/treatments page created and styled

--- src/static-data/treatments.ts
+++ src/static-data/treatments.ts
@@ -2,31 +2,31 @@ export const treatments = [
     {
       id: "1",
       title: "Organic Weed Treatment",
-      image: "/images/organic-weed-treatment.jpg",
+      image: "https://www.mdanderson.org/publications/focused-on-health/May2016/lawncare-cancer-risk/jcr:content/featuredImage.resize.2888.0.high.dir.jpg/1542758619981.jpg",
       description: "Eco-friendly weed suppression methods using natural herbicides and mulching."
     },
     {
       id: "2",
       title: "Insect Control",
-      image: "/images/insect-treatment.jpg",
+      image: "https://www.freshwatersystems.com/cdn/shop/articles/spraying_lawn_pesticide.jpg?v=1714652726&width=500",
       description: "Targeted insecticide applications to protect plants from common pests like aphids, beetles, and grubs."
     },
     {
       id: "3",
       title: "Fertilizer Boost",
-      image: "/images/fertilizer-boost.jpg",
+      image: "https://www.thespruce.com/thmb/-WzGt4S6-HYedqh1sZBplxG8q_8=/3000x0/filters:no_upscale():max_bytes(150000):strip_icc()/when-to-apply-spring-fertilizer-2152974_01-84d78e89fd714fe39e6c3bfc58ece5f6.jpg",
       description: "Seasonal lawn fertilization with slow-release nutrients to ensure consistent growth."
     },
     {
       id: "4",
       title: "Soil Conditioning",
-      image: "/images/soil-treatment.jpg",
+      image: "https://www.thespruce.com/thmb/G3ZBUHOMyIUfVHTNBYvVSQkRJNk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/SPR-how-to-use-gypsum-in-gardening-5079656-hero-d3f45e9a91734ccc9969a31be9d60841.jpg",
       description: "Improving soil structure with compost, gypsum, and organic matter to enhance plant health."
     },
     {
       id: "5",
       title: "Disease Control",
-      image: "/images/disease-treatment.jpg",
+      image: "https://callturfsup.com/wp-content/uploads/2024/12/technician-lawn-spraying-15-1024x682.webp",
       description: "Fungal and bacterial treatment plans to address lawn and shrub diseases."
     }
   ];
